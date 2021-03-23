82ade4d0876b6ef721625916b391e416cfc0291b
MederD <freenetscaper@yahoo.com>
2021-03-22 21:36:03 -0700

    modified outputs

--- terraform_hw4/public_instance/outputs.tf
+++ terraform_hw4/public_instance/outputs.tf
@@ -2,3 +2,13 @@ output "instance_public_ip" {
   value       = aws_instance.my_pub_instance.*.public_ip
   description = "The public IP address of the public server instance."
 }
+
+output "instance_id" {
+  value       = aws_instance.my_pub_instance.*.id
+  description = "The ids of the public server instance."
+}
+
+output "instance_arn" {
+  description = "List of ARNs of instances"
+  value       = aws_instance.my_pub_instance.*.arn
+}
